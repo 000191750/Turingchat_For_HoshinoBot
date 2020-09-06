35506dd0b0a1f598a5b10ef871b98bdcf58d7108
Monkey <184118374@qq.com>
2020-09-06 21:33:17 +0800

    Update and rename turingchat.py to turingchat/turingchat.py

--- turingchat/turingchat.py
+++ turingchat/turingchat.py
@@ -8,9 +8,10 @@ sv_help = '''
 [查看回话概率]查看回话概率
 '''.strip()
 
-sv = Service('turingchat', manage_priv=priv.SUPERUSER, enable_on_default=False, visible=True,help_=sv_help, bundle='图灵帮助')
+sv = Service('turingchat', manage_priv=priv.SUPERUSER, enable_on_default=False, visible=True,help_=sv_help, bundle='图灵')
 
 repeatFrequency = 0.1
+#这里设置机器人回复的概率
 
 @sv.on_message('group')
 async def tulingchat(bot, ctx):
@@ -26,6 +27,7 @@ async def tulingchat(bot, ctx):
 		},
 		'userInfo': {
 			'apiKey': '7508cb12fdb54ec7896d193e4ef10ffe',
+			#在这里填写你的图灵的apikey
 			'userId': ctx['user_id'],
 			#'groupId': ctx['group_id']
 		}
@@ -40,23 +42,7 @@ async def tulingchat(bot, ctx):
 			if result['resultType'] == 'text':
 				await bot.send(ctx, result['values']['text'])
 
-@sv.on_prefix('回话概率')
-async def repeat_frequency(bot, ev):
-	if not priv.check_priv(ev, priv.SUPERUSER):
-		return
-	keyword = ev.message.extract_plain_text()
-	if keyword and keyword.replace(".",'').isdigit():
-		f = float(keyword)
-		if f and not (f > 0.5 or f < 0):
-			global repeatFrequency
-			repeatFrequency = f
-			await bot.send(ev, f'概率已设置为{repeatFrequency}')
-		else:
-			await bot.send(ev, f'{keyword}不是合法的值，请输入一个0-0.5之间的小数')
-	else:
-		await bot.send(ev, f'{keyword}不是数字，请输入一个0-0.5之间的小数')
-
 @sv.on_fullmatch('查看回话概率')
-async def check_frequency(bot, ev):
-	await bot.send(ev, f'我有{repeatFrequency}的几率会说话。')
+async def check_frequency(bot, ctx):
+	await bot.send(ctx, f'我有{repeatFrequency}的几率会说话。')
 
